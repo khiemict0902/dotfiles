cfdbc0d7d5c819a4a657a1f0f3b5fc3a4461f399
khiemict0902 <khiem0902@gmail.com>
2024-03-27 15:39:19 +0700

    update: waybar, hypr, nvim

---
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        --"black",
+        --"debugpy",
+        --"mypy",
+        --"ruff",
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
